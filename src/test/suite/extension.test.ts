296a1da8f548c4f6156f99daa9c9362fe601bc6e
José Fernando Santos  de Carvalho <jfsc@cin.ufpe.br>
2020-08-27 18:42:10 -0300

    windowspath - construcao de diretorio temporario

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,29 @@
-import * as assert from 'assert';
+/* eslint-disable no-undef */
+/* eslint-disable semi */
+// import * as assert from 'assert';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import * as myExtension from '../../extension';
+const fs = require('fs');
+const workingwithpath = require('path');
+const homedir = require('os').homedir();
+const ncp = require('ncp').ncp;
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
-
-	test('Sample test', () => {
-		assert.equal(-1, [1, 2, 3].indexOf(5));
-		assert.equal(-1, [1, 2, 3].indexOf(0));
-	});
+suite('Deploy Classic Metadata', () => {
+  vscode.window.showInformationMessage('Start all tests.');
+  test('Spaced path', () => {
+    try {
+      const dir = workingwithpath.normalize(`${homedir}/.sfci/tmp/Projeto X - foo bar/REPO_NAME/src-files`);
+      const wspace = vscode.workspace.rootPath;
+      const metadatasample = workingwithpath.normalize(`${wspace}/resources/metadata`);
+      const uri = vscode.Uri.parse(`${dir}/metadata/package.xml`);
+      fs.mkdir(dir, { recursive: true }, (err:any) => { throw err; });
+      ncp(`${metadatasample}`, `${dir}`);
+      myExtension.deployPack(uri);
+    } catch (error) {
+      console.log(error)
+    }
+  });
 });
